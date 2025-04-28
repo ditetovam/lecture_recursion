dc0a815687f7830c322c074b22671debdc0b4bbf
Marketa Ditetova <271339@vut.cz>
2025-04-28 09:28:15 +0200

    new algoritm

--- fibo.py
+++ fibo.py
@@ -1,7 +1,17 @@
+def recursive_nth_fibo(idx):
+    if idx == 0 or idx == 1:
+        return idx
+    else:
+        return recursive_nth_fibo(idx - 1) + recursive_nth_fibo(idx - 2)
 
-def main():
-    pass
+def main(number):
+    print(recursive_nth_fibo(number))
+    posloupnost = []
+    for num in range(number + 1):
+        n = recursive_nth_fibo(num)
+        posloupnost.append(n)
+    return print(posloupnost)
 
 
 if __name__ == "__main__":
-    main()
+    main(5)
